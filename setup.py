3bc07edc315739fc77abb4eeb91afe792df5a949
Jay Greguske <jgregusk@redhat.com>
2015-06-23 12:02:00 -0400

    steal ownership in setup.py, include other scripts (cherry picked from commit 2e0e3d9b0132fea20432925139fa79bddf0a333f)

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, Command
 
 setup(
     name = "dockpulp",
-    version = "0.1",
-    author = "Release Configuration Management",
-    author_email = "rcm-tools@redhat.com",
+    version = "1.11",
+    author = "Jay Greguske",
+    author_email = "jgregusk@redhat.com",
     description = ("ReST API Client to Pulp for manipulating docker images"),
     license = "GPLv3",
     url = "https://github.com/release-engineering/dockpulp.git",
     package_dir = {'': '.'},
     packages = ['dockpulp'],
-    scripts = ['bin/dock-pulp.py'],
+    scripts = ['bin/dock-pulp.py', 'bin/dock-pulp-bootstrap.py', 'bin/dock-pulp-restore.py'],
     data_files = [('/etc', ['conf/dockpulp.conf'])],
 )
