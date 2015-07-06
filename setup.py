1cc4cecfd487e39e77cd300d2bf63af95885b83e
Tim Waugh <twaugh@redhat.com>
2015-07-06 14:59:53 +0100

    Add simplejson as a requirement for Python 2.6

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 import sys
 from setuptools import setup, Command
 
+def _simplejson_on_python26():
+    if (sys.version_info[0] > 2 or
+        (sys.version_info[0] == 2 and
+         sys.version_info[1] > 6)):
+        return []
+    return ['simplejson']
+
 setup(
     name = "dockpulp",
     version = "1.11",
@@ -12,6 +19,7 @@ setup(
     url = "https://github.com/release-engineering/dockpulp.git",
     package_dir = {'': '.'},
     packages = ['dockpulp'],
+    install_requires = _simplejson_on_python26(),
     scripts = ['bin/dock-pulp.py', 'bin/dock-pulp-bootstrap.py', 'bin/dock-pulp-restore.py'],
     package_data = {'': ['conf/dockpulp.conf']},
 )
