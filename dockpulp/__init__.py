77c9cd73e7e2fec90873593df4b67f83138bb4f5
Brendan Reilly <breilly@redhat.com>
2016-03-02 10:32:06 -0500

    Fixed indentation error

--- dockpulp/__init__.py
+++ dockpulp/__init__.py
@@ -467,8 +467,8 @@ class Pulp(object):
         log.debug('search request:')
         log.debug(json.dumps(data))
         try:
-        img = self._post('/pulp/api/v2/repositories/%s/search/units/' % HIDDEN,
-            data=json.dumps(data))[0]
+            img = self._post('/pulp/api/v2/repositories/%s/search/units/' % HIDDEN,
+                             data=json.dumps(data))[0]
         except IndexError:
             log.info('missing parent layer %s', iid)
             log.info('skipping layer')
